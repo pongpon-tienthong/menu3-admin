b1147531139243f1ee170c042d4403f4ac61cfe6
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-08-29 19:48:55 -0400

    Initialize the new project

---
+++ src/Menu3Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Menu3Admin extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Menu3Admin;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Menu3Admin from './Menu3Admin';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Menu3Admin />, document.getElementById('root'));
+registerServiceWorker();
