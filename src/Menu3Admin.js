080f061abb5f589ec2b812ab7af93fa4a819a1db
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-08-29 20:12:03 -0400

    Set up Material UI

--- src/Menu3Admin.js
+++ src/Menu3Admin.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Layout from "./components/Layout/Layout";
 import RestaurantScreen from "./screens/RestaurantScreen/RestaurantScreen";
 import MenuScreen from "./screens/MenuScreen/MenuScreen";
 
+import 'typeface-roboto'
+
 class Menu3Admin extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            <Route path="/restaurant/:restaurantId/menu" component={MenuScreen} />
-            <Route exact path="/" component={RestaurantScreen} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
+      <Fragment>
+        <CssBaseline />
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+              <Route path="/restaurant/:restaurantId/menu" component={MenuScreen} />
+              <Route exact path="/" component={RestaurantScreen} />
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+      </Fragment>
     );
   }
 }
