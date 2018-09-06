347669c587164d0b524e9de40b46decae97c9c0b
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-09-06 00:18:14 -0400

    Add dropzone

--- src/components/Restaurant/RestaurantForm/RestaurantForm.js
+++ src/components/Restaurant/RestaurantForm/RestaurantForm.js
@@ -7,7 +7,9 @@ import TextField from '@material-ui/core/TextField';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
-import { Maximize, Minimize, Close } from '@material-ui/icons';
+import IconButton from "@material-ui/core/IconButton";
+import { Maximize, Minimize, Close, AddPhotoAlternate, Delete } from '@material-ui/icons';
+import Dropzone from 'react-dropzone';
 
 import CustomButton from "../../UI/CumtomButton/CustomButton";
 
@@ -62,12 +64,53 @@ const styles = theme => ({
   },
   textField: {
     padding: theme.spacing.unit
+  },
+  addPhotoIcon: {
+    width: 100,
+    height: 100,
+    fill: theme.palette.grey[400]
+  },
+  dropZone: {
+    width: 150,
+    height: 150,
+    borderWidth: 5,
+    borderColor: theme.palette.grey[400],
+    borderStyle: 'dashed',
+    borderRadius: 5
+  },
+  previewImage: {
+    width: 150,
+    height: 150,
+  },
+  centerChild: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative'
+  },
+  deleteButton: {
+    position: 'absolute',
+    right: 0,
+    // bottom: 0
   }
 });
 
 class RestaurantForm extends Component {
   state = {
-    minimize: false
+    minimize: false,
+    imageFiles: null
+  }
+
+  handleDropFile = imageFiles => {
+    this.setState({
+      imageFiles: imageFiles
+    });
+  }
+
+  handleDeleteFile = () => {
+    this.setState({
+      imageFiles: null
+    });
   }
 
   handleCreateRestaurant = () => {
@@ -123,7 +166,7 @@ class RestaurantForm extends Component {
           />
           {this.state.minimize ? null :
             <CardContent className={classes.paper}>
-              <Grid container spacing={16}>
+              <Grid container spacing={8}>
                 <Grid item xs={12}>
                   <TextField
                     id="name"
@@ -153,39 +196,67 @@ class RestaurantForm extends Component {
                     autoComplete="billing address-line2"
                   />
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="city"
-                    name="city"
-                    label="City"
-                    fullWidth
-                    autoComplete="billing address-level2"
-                  />
+                <Grid item container spacing={8} xs={12} sm={6}>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      id="city"
+                      name="city"
+                      label="City"
+                      fullWidth
+                      autoComplete="billing address-level2"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      id="zip"
+                      name="zip"
+                      label="Zip / Postal code"
+                      fullWidth
+                      autoComplete="billing postal-code"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      id="payingPriority"
+                      name="payingPriority"
+                      label="Paying Priority"
+                      type="number"
+                      autoComplete="off"
+                      fullWidth
+                    />
+                  </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField id="state" name="state" label="State/Province/Region" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="zip"
-                    name="zip"
-                    label="Zip / Postal code"
-                    fullWidth
-                    autoComplete="billing postal-code"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="payingPriority"
-                    name="payingPriority"
-                    label="Paying Priority"
-                    type="number"
-                    autoComplete="off"
-                    fullWidth
-                  />
+
+                <Grid item container spacing={8} xs={12} sm={6}>
+                  <Grid item xs={12} className={classes.centerChild}>
+                    {this.state.imageFiles ? this.state.imageFiles.map((file) => {
+                      return (
+                        <Fragment key={file.name} >
+                          <img className={classes.previewImage} src={file.preview} />
+                          <CustomButton
+                            variant="fab"
+                            mini
+                            aria-label="Delete"
+                            btnType="danger"
+                            className={classes.deleteButton}
+                            clicked={this.handleDeleteFile}
+                          >
+                            <Delete />
+                          </CustomButton>
+                        </Fragment>
+                      )
+                    }) :
+                      <Dropzone multiple={false} accept=".jpeg, .png" className={classNames(classes.dropZone, classes.centerChild)} onDrop={imageFiles => this.handleDropFile(imageFiles)}>
+                        <AddPhotoAlternate className={classes.addPhotoIcon} />
+                      </Dropzone>
+                    }
+                  </Grid>
                 </Grid>
               </Grid>
               <div className={classes.buttons}>
