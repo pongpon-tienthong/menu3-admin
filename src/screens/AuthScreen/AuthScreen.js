b0c7ab9d391cdb0417d1dc2df7ce36aaa0d1938e
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-09-19 19:53:49 -0400

    Adjust spacing

--- src/screens/AuthScreen/AuthScreen.js
+++ src/screens/AuthScreen/AuthScreen.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-import LockIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -28,7 +26,7 @@ const styles = theme => ({
     },
   },
   paper: {
-    marginTop: theme.spacing.unit * 8,
+    marginTop: theme.spacing.unit * 16,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -89,9 +87,9 @@ class AuthScreen extends Component {
     }
 
     return (
-      <React.Fragment>
+      <Fragment>
         {authRedirect}
-        <CssBaseline />
+        {/* <CssBaseline /> */}
         <main className={classes.layout}>
           <Paper className={classes.paper}>
             <div className={classes.header}>
@@ -137,7 +135,7 @@ class AuthScreen extends Component {
             </form>
           </Paper>
         </main>
-      </React.Fragment>
+      </Fragment>
     );
   }
 }
